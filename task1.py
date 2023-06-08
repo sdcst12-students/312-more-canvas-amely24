1d3517b24ca963d09f5b94ac9d8f879204cfc458
amely24 <124203331+amely24@users.noreply.github.com>
2023-06-08 10:56:32 -0700

    jj

--- task1.py
+++ task1.py
@@ -10,6 +10,18 @@ w.attributes('-topmost',True)
 c = tk.Canvas(height=475,width=900,bg="#ffdddd")
 c.pack()
 f = open('map1.txt')
+fr=f.read()
+frs=fr.split('\n')
+print(frs)
 
 
+maze=[]
+
+
+for k in range(0,15):
+    for (i,j) in itertools.zip_longest(frs[k],range(len(frs[k]))):
+        if i=='*':
+            maze.append(c.create_rectangle(0+j*31,0+k*31,31+j*31,31+k*31,fill='#000000'))
+        else:
+            pass
 w.mainloop()
